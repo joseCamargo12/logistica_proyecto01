2ec86bdb0b8b803b8972fc5f2abe3348e9e5aa29
José Camargo <camargojose28@hotmail.com>
2025-07-23 20:41:18 -0500

    Primer commit

--- src/ver_head.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-archivo = 'output/operaciones_limpio.csv'
-df = pd.read_csv(archivo)
-
-print("📋 Primeras filas del archivo:")
-print(df.head(10))  # Puedes cambiar el número si quieres ver más
